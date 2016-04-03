92ca81d5a814a122d0d96d26d46fc7472cb9bf8a
AsanoYuki <yuuki.1224.softtennis@gmail.com>
2016-04-03 15:26:47 -0700

    Update

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   has_many :projects
   belongs_to :user
   enum category: {wear: 0, eat: 1, touch: 2, travel: 3, listen: 4}
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/account/account-icon.png"
+  has_attached_file :image, styles: { product_page: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/account/account-icon.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
